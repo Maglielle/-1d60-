03bdcce3deaf1ae1ec047b4a069bf2a797c3e1c1
RRST-JP <depust60+github@gmail.com>
2025-01-17 15:28:25 +0900

    Add files via upload

--- weapon_selector_app.py
+++ weapon_selector_app.py
@@ -12,8 +12,11 @@ weapons = [
 elements = ["火", "水", "土", "風", "光", "闇"]
 
 def generate_roll_mapping():
+    # 属性のリストを武器の数に合わせて繰り返す
+    extended_elements = elements * (len(weapons) // len(elements)) + elements[:len(weapons) % len(elements)]
+    
     # 武器と属性をシャッフルして1対1で割り当て
-    shuffled_elements = random.sample(elements, len(weapons))
+    shuffled_elements = random.sample(extended_elements, len(weapons))
     shuffled_weapons = random.sample(weapons, len(weapons))
     
     # シャッフルされたリストを使って、1対1でマッピング
